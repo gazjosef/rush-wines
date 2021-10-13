986b44288e71dbbd2edda797021683fc0951f7f2
Gareth Hind <garethjhind@gmail.com>
2021-10-13 18:59:35 +1100

    Updated Script

--- assets/custom-scripts.js
+++ assets/custom-scripts.js
@@ -1,30 +1,30 @@
-{% javascript %} 
-let slideIndex = 0;
+// {% javascript %}
+// let slideIndex = 0;
 
-function showSlides() {
-  let slides = document.getElementsByClassName("slideshow__slides");
-  let dots = document.getElementsByClassName("dots__dot");
-  let i;
+// function showSlides() {
+//   let slides = document.getElementsByClassName("slideshow__slides");
+//   let dots = document.getElementsByClassName("dots__dot");
+//   let i;
 
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
 
-  slideIndex++;
+//   slideIndex++;
 
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
+//   if (slideIndex > slides.length) {
+//     slideIndex = 1;
+//   }
 
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
 
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " active";
 
-  setTimeout(showSlides, 3000); // Change image every 3 seconds
-}
+//   setTimeout(showSlides, 3000); // Change image every 3 seconds
+// }
 
-showSlides();
-{% endjavascript %}
+// showSlides();
+// {% endjavascript %}
